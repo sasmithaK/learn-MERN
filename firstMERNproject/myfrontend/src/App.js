31cc9b5be3fe428a017344829a797799169bb7db
Sasmitha Kavindu <73338956+sasmithaK@users.noreply.github.com>
2024-06-25 10:44:54 +0530

    messed up
    - dependency vulnerabilities

--- firstMERNproject/myfrontend/src/App.js
+++ firstMERNproject/myfrontend/src/App.js
@@ -1,12 +1,18 @@
 import Header from './comp/Header';
 import AddStudent from './comp/AddStudent';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <AddStudent/>
-    </div>
+    <Router>
+      <div>
+        <Header/>
+
+        <Route path="/add" component={AddStudent}/>
+
+        <AddStudent/>
+      </div>
+    </Router>
   );
 }
 
